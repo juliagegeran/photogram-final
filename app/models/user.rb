d54167ad038996b011ad63fe66dd2380ed67f8b5
Julia Ran <59769618+juliagegeran@users.noreply.github.com>
2023-11-22 23:40:11 +0000

    photos#likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,6 @@ class User < ApplicationRecord
          has_many  :comments, foreign_key: "author_id", dependent: :destroy
          has_many  :likes, foreign_key: "fan_id", dependent: :destroy
          has_many  :follow_sent, class_name: "FollowRequest", foreign_key: "sender_id", dependent: :destroy
-         has_many(:liked_photos, through: :likes, source: :photo) 
          has_many  :follow_received, class_name: "FollowRequest", foreign_key: "recipient_id", dependent: :destroy
          
 end
