1201b0fac362dbfa5659319f1044e1dd13718048
Julia Ran <59769618+juliagegeran@users.noreply.github.com>
2023-11-22 23:08:12 +0000

    through accessor

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,8 +29,8 @@ class User < ApplicationRecord
          has_many  :photos, foreign_key: "owner_id", dependent: :destroy
          has_many  :comments, foreign_key: "author_id", dependent: :destroy
          has_many  :likes, foreign_key: "fan_id", dependent: :destroy
-         has_many :liked_photos, through: :likes, source: :photo
          has_many  :follow_sent, class_name: "FollowRequest", foreign_key: "sender_id", dependent: :destroy
+         has_many(:liked_photos, through: :likes, source: :photo) 
          has_many  :follow_received, class_name: "FollowRequest", foreign_key: "recipient_id", dependent: :destroy
          
 end
