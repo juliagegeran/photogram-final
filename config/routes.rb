27491f4bec57a3eb3aebca2c896f9bd8619378f2
Julia Ran <59769618+juliagegeran@users.noreply.github.com>
2023-11-26 20:05:45 +0000

    final bugs seems unfixable

--- config/routes.rb
+++ config/routes.rb
@@ -34,11 +34,6 @@ Rails.application.routes.draw do
 
   # CREATE
   post("/insert_like", { :controller => "likes", :action => "create" })
-          
-  # READ
-  get("/likes", { :controller => "likes", :action => "index" })
-  
-  get("/likes/:path_id", { :controller => "likes", :action => "show" })
   
   # UPDATE
   
@@ -54,17 +49,6 @@ Rails.application.routes.draw do
   # CREATE
   post("/insert_comment", { :controller => "comments", :action => "create" })
           
-  # READ
-  get("/comments", { :controller => "comments", :action => "index" })
-  
-  get("/comments/:path_id", { :controller => "comments", :action => "show" })
-  
-  # UPDATE
-  
-  post("/modify_comment/:path_id", { :controller => "comments", :action => "update" })
-  
-  # DELETE
-  get("/delete_comment/:path_id", { :controller => "comments", :action => "destroy" })
 
   #------------------------------
 
